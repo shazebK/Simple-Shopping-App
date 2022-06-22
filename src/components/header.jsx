3850d064c1b980a5ee04d4cdf1444705d334e90a
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2022-06-22 12:48:58 +0530

    add navigation menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function Header(){
     return (
-         <div>
-             
+         <div className = "nav">
+            <h1>TakeIt</h1>
+            <form>
+                <input type = "text" placeholder="Search product" autoComplete="off"/>
+                <button type = "submit"><span className="material-symbols-outlined">search</span></button>
+            </form>
+            <a><span className="material-symbols-outlined cart-icon">shopping_cart</span></a>
          </div>
     );
 }
