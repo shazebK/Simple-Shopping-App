921af1855dc0e3ea8c18622d756ba0c2616f7106
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2022-06-23 18:58:17 +0530

    Add Search Functionality

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import Header from "./header";
 import Product from "./product";
 import rawData from "../product_dummy_data.json";
 
 function App(){
    const products = rawData.products;
+
+   const [searchTerm,setSearchTerm] = useState("");
+
+   function handleSearch(Term){
+       setSearchTerm(Term);
+   }
+
    return (
        <div>
-       <Header/>
-       {products.map(product => <Product key = {product.id} prod = {product}/>)}
+       <Header onSearch = {handleSearch}/>
+       {products.filter((product) => {
+           if(searchTerm == "")
+           return product;
+           else if(product.title.toLowerCase().includes(searchTerm.toLowerCase()))
+           return product;
+       }).map((product,key) => <Product key = {product.id} prod = {product}/>)}
        </div>
    );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 
-function Header(){
+function Header(props){
+    const [searchTerm,setSearchTerm] = useState("");
+
+    function handleChange(event){
+        const newSearchTerm = event.target.value;
+        setSearchTerm(newSearchTerm);
+    
+        props.onSearch(newSearchTerm);
+    }
+
     return (
          <div className = "nav">
             <h1>TakeIt</h1>
             <form>
-                <input type = "text" placeholder="Search product" autoComplete="off"/>
+                <input type = "text" onChange = {handleChange} placeholder="Search product" value = {searchTerm} autoComplete="off"/>
                 <button type = "submit"><span className="material-symbols-outlined">search</span></button>
             </form>
             <a><span className="material-symbols-outlined cart-icon">shopping_cart</span></a>
