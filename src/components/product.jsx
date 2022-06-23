4b3671877a5681f1ae69f9dd48d2211a7aef71e6
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2022-06-23 12:33:13 +0530

    add Dummy data Json file

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Product(props){
     return (
         <div className = "product">
+        <div className="content">
         <div>
             <img className = "product-thumbnail" src = {props.prod.thumbnail} alt = {props.title}/>
         </div>
@@ -11,6 +12,7 @@ function Product(props){
         <h5>{props.prod.description}</h5>
         <h4>Discount Applied : <span>{props.prod.discountPercentage}%</span></h4>
         <h4>Ratings : {props.prod.rating} <i className="material-symbols-outlined"> star</i></h4>
+        </div>
         </div>
         <h3>${props.prod.price}</h3>
         </div>
