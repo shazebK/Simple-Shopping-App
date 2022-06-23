5b44da4acd6357c0a812a4753736d6a6d6cc92f0
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2022-06-23 20:12:18 +0530

    add static-search

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,16 +6,19 @@ function Header(props){
     function handleChange(event){
         const newSearchTerm = event.target.value;
         setSearchTerm(newSearchTerm);
-    
-        props.onSearch(newSearchTerm);
+    }
+
+    function handleSubmit(event){
+        props.onSearch(searchTerm);
+        event.preventDefault();
     }
 
     return (
          <div className = "nav">
             <h1>TakeIt</h1>
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <input type = "text" onChange = {handleChange} placeholder="Search product" value = {searchTerm} autoComplete="off"/>
-                <button type = "submit"><span className="material-symbols-outlined">search</span></button>
+                <button><span className="material-symbols-outlined">search</span></button>
             </form>
             <a><span className="material-symbols-outlined cart-icon">shopping_cart</span></a>
          </div>
